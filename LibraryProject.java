ba2aea684a67a7c73390c35bb5077eaa4842c1f0
Yash Garg <62419230+mr-mods-yg@users.noreply.github.com>
2023-04-16 23:08:57 +0530

    Add files via upload

--- LibraryProject.java
+++ LibraryProject.java
@@ -2,52 +2,64 @@ package com.company;
 
 import java.util.Scanner;
 
-interface Library{
-    void addBook(String []b,String a);
-    void issueBook(String []b,String[]c, String a);
-    void returnBook(String []b,String[]c, String a);
-    void showAvailableBooks(String []b);
+interface Library {
+    void addBook(String a);
+
+    void issueBook(String a);
+
+    void returnBook(String a);
+
+    void showAvailableBooks();
 }
-public class LibraryProject implements Library{
+
+class MyLibraryProject implements Library {
+    private String[] bookList;
+    private String[] issuebookList;
+
+    MyLibraryProject() {
+        this.bookList = new String[100];
+        this.issuebookList = new String[100];
+    }
+
     // You have to implement a library using Java Class "Library"
     // Methods: addBook, issueBook, returnBook, showAvailableBooks
     // Properties: Array to store the available books,
     // Array to store the issued books
     @Override
-    public void addBook(String[] bookList,String bookName){
-        for (int i=0;i<bookList.length;i++){
-            if (bookList[i]==null){
-                bookList[i]=bookName;
+    public void addBook(String bookName) {
+        for (int i = 0; i < this.bookList.length; i++) {
+            if (this.bookList[i] == null) {
+                this.bookList[i] = bookName;
                 break;
             }
         }
     }
 
     @Override
-    public void showAvailableBooks(String[] bookList) {
-        for (String i:bookList){
-            if(i!=null) {
+    public void showAvailableBooks() {
+        for (String i : this.bookList) {
+            if (i != null) {
                 System.out.println(i);
             }
         }
     }
 
     @Override
-    public void issueBook(String[] booklist, String[] issuebooklist, String bookName) {
+    public void issueBook(String bookName) {
         int addtoissue = 0;
-        for (int i=0;i<booklist.length;i++){
-            if (booklist[i]!=null){
-                if (booklist[i].equals(bookName)){
-                    booklist[i]=null;
+        for (int i = 0; i < this.bookList.length; i++) {
+            if (this.bookList[i] != null) {
+                if (this.bookList[i].equals(bookName)) {
+                    this.bookList[i] = null;
                     addtoissue = 1;
                     break;
                 }
             }
         }
-        if (addtoissue==1){
-            for (int i=0;i<booklist.length;i++){
-                if (issuebooklist[i]==null){
-                    issuebooklist[i]=bookName;
+        if (addtoissue == 1) {
+            for (int i = 0; i < this.bookList.length; i++) {
+                if (this.issuebookList[i] == null) {
+                    this.issuebookList[i] = bookName;
                     break;
                 }
             }
@@ -55,47 +67,40 @@ public class LibraryProject implements Library{
     }
 
     @Override
-    public void returnBook(String[] booklist, String[] issuebooklist, String bookName) {
+    public void returnBook(String bookName) {
         int removefromissue = 0;
-        for (int i=0;i<issuebooklist.length;i++){
-            if (issuebooklist[i]!=null){
-                if (issuebooklist[i].equals(bookName)){
-                    issuebooklist[i]=null;
+        for (int i = 0; i < this.issuebookList.length; i++) {
+            if (this.issuebookList[i] != null) {
+                if (this.issuebookList[i].equals(bookName)) {
+                    this.issuebookList[i] = null;
                     removefromissue = 1;
                     break;
                 }
             }
         }
-        if (removefromissue==1){
-            for (int i=0;i<booklist.length;i++){
-                if (booklist[i]==null){
-                    booklist[i]=bookName;
+        if (removefromissue == 1) {
+            for (int i = 0; i < this.bookList.length; i++) {
+                if (this.bookList[i] == null) {
+                    this.bookList[i] = bookName;
                     break;
                 }
             }
         }
+
     }
+    public void showissuedBooks () {
+        for (String i : this.issuebookList) {
+            if (i != null) {
+                System.out.println(i);
+            }
+        }
+    }
+}
 
+public class LibraryProject {
     public static void main(String[] args) {
-        String[] bookList = new String[100];
-        String[] issuedBookList = new String[100];
-
-        LibraryProject LP = new LibraryProject();
-//        LP.addBook(bookList,"The Reality of Money");
-//        LP.addBook(bookList,"Coding Master v4");
-//        LP.addBook(bookList,"Pirates of Carribean");
-//        System.out.println("\nCurrently Available Books :");
-//        LP.showAvailableBooks(bookList);
-//        System.out.println("\nIssuing the books ..");
-//        LP.issueBook(bookList,issuedBookList,"The Reality of Money");
-//        System.out.println("\nCurrent Books :");
-//        LP.showAvailableBooks(bookList);
-//        System.out.println("\nFetching the returned books ..");
-//        LP.returnBook(bookList,issuedBookList,"The Reality of Money");
-//        System.out.println("\nCurrent Books :");
-//        LP.showAvailableBooks(bookList);
-
-        while(true){
+        MyLibraryProject LP = new MyLibraryProject();
+        while (true) {
             System.out.println("--------------------------------------------");
             System.out.println("Choose The Options Given Below :");
             System.out.println("1. Add a Book to the Library");
@@ -111,44 +116,45 @@ public class LibraryProject implements Library{
             String bookName;
             bookName = sc.nextLine();
             int Exit = 0;
-            switch (x){
+            switch (x) {
                 case 1:
                     System.out.println("Enter the Name of the book :");
                     bookName = sc.nextLine();
-                    LP.addBook(bookList,bookName);
+                    LP.addBook(bookName);
                     System.out.println("Book Added !");
                     break;
                 case 2:
                     System.out.println("Enter the Name of the book :");
                     bookName = sc.nextLine();
-                    LP.issueBook(bookList,issuedBookList,bookName);
+                    LP.issueBook(bookName);
                     System.out.println("Book Issued Succesfully !");
                     break;
                 case 3:
                     System.out.println("Enter the Name of the book :");
                     bookName = sc.nextLine();
-                    LP.returnBook(bookList,issuedBookList,bookName);
+                    LP.returnBook(bookName);
                     System.out.println("Book Returned Succesfully !");
                     break;
                 case 4:
                     System.out.println("Total Available Books :");
-                    LP.showAvailableBooks(bookList);
+                    LP.showAvailableBooks();
                     break;
                 case 5:
                     System.out.println("Total Already Issued Books :");
-                    LP.showAvailableBooks(issuedBookList);
+                    LP.showissuedBooks();
                     break;
                 case 6:
                     System.out.println("Thanks For Visiting.");
-                    Exit =1;
+                    Exit = 1;
                     break;
                 default:
                     System.out.println("Wrong Input");
                     break;
             }
-            if (Exit==1){
+            if (Exit == 1) {
                 break;
             }
         }
     }
 }
+
